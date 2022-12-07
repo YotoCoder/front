b10f01e82fa7927ad21af1111cd837471681f5cb
YotoCoder <yotoelectronics@gmail.com>
2022-12-07 01:56:07 -0500

    stats

--- pages/user/profile/[username].js
+++ pages/user/profile/[username].js
@@ -32,6 +32,16 @@ const Username = () => {
   const [user, setUser] = useState({});
   const [mmr, setMmr] = useState(null);
 
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [topHeroes, setTopHeroes] = useState([]);
+
+  const [heroes, setHeroes] = useState([]);
+
+  const [hero_1, setHero_1] = useState({});
+  const [hero_2, setHero_2] = useState({});
+  const [hero_3, setHero_3] = useState({});
+
   const [session, setSession] = useAtom(sessionAtom);
   const [usernameSession, setUsernameSession] = useAtom(usernameSessionAtom);
 
@@ -52,14 +62,57 @@ const Username = () => {
   }, [username]);
 
   useEffect(() => {
-    if (user.id_amigo != undefined) {
-      axios.get(`https://api.opendota.com/api/players/${user.id_amigo}`).then((res) => {
+    if (
+      user.id_amigo != undefined &&
+      user.id_amigo != "null" &&
+      user.id_amigo != ""
+    ) {
+      axios
+        .get(`https://api.opendota.com/api/players/${user.id_amigo}/`)
+        .then((res) => {
           setMmr(res.data.mmr_estimate.estimate);
-          console.log(res.data.mmr_estimate.estimate);
           setCargando(false);
-          });
+        });
+
+      axios
+        .get(`https://api.opendota.com/api/players/${user.id_amigo}/wl/`)
+        .then((res) => {
+          setWins(res.data.win);
+          setLosses(res.data.lose);
+        });
+
+      axios
+        .get(`https://api.opendota.com/api/players/${user.id_amigo}/heroes/`)
+        .then((res) => {
+          setTopHeroes(res.data);
+        });
+
+      axios.get(`https://api.opendota.com/api/heroStats/`).then((res) => {
+        console.log(res.data);
+        setHeroes(res.data);
+      });
     }
-  }, [user.id_amigo]);
+  }, [user]);
+
+  useEffect(() => {
+    if (heroes != []) {
+      heroes.map((hero) => {
+        // console.log(topHeroes[0].hero_id)
+        // console.log(hero.id)
+
+        if (topHeroes[0].hero_id == hero.id) {
+          setHero_1(hero);
+        }
+        if (topHeroes[1].hero_id == hero.id) {
+          setHero_2(hero);
+        }
+
+        if (topHeroes[2].hero_id == hero.id) {
+          setHero_3(hero);
+        }
+      });
+    }
+  }, [topHeroes]);
 
   return (
     <Suspense fallback={<Cargador />}>
@@ -112,7 +165,7 @@ const Username = () => {
       <div className="lg:flex lg:flex-col lg:items-center lg:justify-center">
         <div className="lg:flex">
           {/* user profile*/}
-          <div className="flex relative lg:w-96 mt-20 flex-col card items-center justify-center w-auto m-4">
+          <div className="flex relative lg:w-[500px] mt-20 flex-col card items-center justify-center w-auto m-4">
             <div className="flex flex-col items-center justify-center relative top-[-7vh] pt-[-7vh] w-52 ">
               <div className="">
                 <img
@@ -131,11 +184,7 @@ const Username = () => {
               </div>
 
               {username == usernameSession ? (
-                <Link
-                  href={"/user/form/" + username}
-                  // href={'#'}
-                  // as={`/user/form/${username}`}
-                >
+                <Link href={"/user/form/" + username}>
                   <img
                     src="../../icons/edit.svg"
                     alt="menu-editar"
@@ -158,14 +207,112 @@ const Username = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col items-start justify-start">
-                <p className="text-gray-400 text-base pt-4">
-                  Roll: {user.roll ? user.roll : "Sin rol"}
-                </p>
+              <div className="flex flex-col items-start justify-center">
+              <div>
+                  <p className="text-gray-400 text-base pt-4">
+                    Roll: {user.roll ? user.roll : "Sin rol"}
+                  </p>
+                  <p className="text-gray-400 text-base">
+                    MMR avg: {mmr ? mmr : "Sin id amigo o steam privado"}
+                  </p>
 
-                <p className="text-gray-400 text-base">
-                  MMR avg: {mmr ? mmr : "Sin id amigo o steam privado"}
-                </p>
+                  <p className="text-gray-400 text-base pt-2">
+                    Wins: {wins ? wins : ""}
+                  </p>
+
+                  <p className="text-gray-400 text-base">
+                    Losses: {losses ? losses : ""}
+                  </p>
+                </div>
+                <div className="bg-[#2f3136] flex flex-col w-72 items-center justify-center drop-shadow-md text-gray-100 text-base p-4 m-4 rounded-xl">
+                  <p className="text-bold ">Top 3 Heroes</p>
+                  <div className="flex flex-col gap-4 py-2">
+                    <div className="flex flex-col bg-[#202225] rounded-2xl p-4 items-center justify-center">
+                      {hero_1.localized_name ? hero_1.localized_name : ""}
+                      <img
+                        src={
+                          hero_1.icon
+                            ? "https://api.opendota.com" + hero_1.icon
+                            : "../../icons/loader.svg"
+                        }
+                        alt="hero-1"
+                        className="w-6 h-6 rounded-full"
+                      />
+                      <p className="flex gap-4 items-center justify-start">
+                        {topHeroes[0] && (
+                          <>
+                            <div className="text-green-300">
+                              W <b className="text-white">{topHeroes[0].win}</b>
+                            </div>
+                            <div className="text-red-300">
+                              L{" "}
+                              <b className="text-white">
+                                {topHeroes[0].games - topHeroes[0].win}
+                              </b>
+                            </div>
+                          </>
+                        )}
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col bg-[#202225] rounded-2xl p-4 items-center justify-center">
+                      {hero_2.localized_name ? hero_2.localized_name : ""}
+
+                      <img
+                        src={
+                          hero_2.icon
+                            ? "https://api.opendota.com" + hero_2.icon
+                            : "../../icons/loader.svg"
+                        }
+                        alt="hero-2"
+                        className="w-6 h-6 rounded-full"
+                      />
+                      <p className="flex gap-4 items-center justify-start">
+                        {topHeroes[1] && (
+                          <>
+                            <div className="text-green-300">
+                              W <b className="text-white">{topHeroes[1].win}</b>
+                            </div>
+                            <div className="text-red-300">
+                              L{" "}
+                              <b className="text-white">
+                                {topHeroes[1].games - topHeroes[1].win}
+                              </b>
+                            </div>
+                          </>
+                        )}
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col bg-[#202225] rounded-2xl p-4 items-center justify-center">
+                      {hero_3.localized_name ? hero_3.localized_name : ""}
+                      <img
+                        src={
+                          hero_3.icon
+                            ? "https://api.opendota.com" + hero_3.icon
+                            : "../../icons/loader.svg"
+                        }
+                        alt="hero-3"
+                        className="w-6 h-6 rounded-full"
+                      />
+                      <p className="flex gap-4 items-center justify-start">
+                        {topHeroes[2] && (
+                          <>
+                            <div className="text-green-300">
+                              W <b className="text-white">{topHeroes[2].win}</b>
+                            </div>
+                            <div className="text-red-300">
+                              L{" "}
+                              <b className="text-white">
+                                {topHeroes[2].games - topHeroes[2].win}
+                              </b>
+                            </div>
+                          </>
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <h3 className="about pt-8 pb-4">Sobre Mi</h3>
