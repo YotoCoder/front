f40fbe3bdd444ba9bd481954cdeb9a3b50bc1e8e
Yoto <yoto@dev.com>
2023-01-25 02:32:30 -0500

    15

--- pages/liga/[id].js
+++ pages/liga/[id].js
@@ -559,7 +559,7 @@ const Liga = () => {
                             {jugador.perdidas}
                           </td>
                           <td className="px-1 py-2 text-white border border-[#121212]">
-                            {jugador.perdidas + jugador.ganadas}/12
+                            {jugador.perdidas + jugador.ganadas}/15
                           </td>
                         </tr>
                       ))}
