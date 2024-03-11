fbb4c353c7299bca4991a973ec10f617845191de
Tebes <tprogramacion69@gmail.com>
2024-03-11 00:44:48 -0400

    break all

--- pages/components/Nosotros.js
+++ pages/components/Nosotros.js
@@ -20,7 +20,7 @@ export default function Nosotros() {
             <h2 className='text-gray-300 font-light text-xl'>
                 VMC es un proyecto de Esports en rápido crecimiento. 
             </h2>
-            <h2 className='text-gray-300 font-light text-xl'>
+            <h2 className='text-gray-300 font-light text-xl break-all'>
                 Nuestra misión es popularizar los Esports profesionales y organizar torneos para apoyar a la comunidad Venezolana y Latinoamericana.<br></br><br></br>
                 Nuestro objetivo es proporcionar una plataforma en línea segura y justa para que los jugadores de Dota 2 puedan competir y mejorar sus habilidades.<br></br><br></br>
                 Estamos comprometidos a mantener un entorno de juego amistoso y respetuoso para todos los participantes.
