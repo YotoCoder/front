6735a342186e9d8a6847cc2f601bff927ad9000e
Tebes <tprogramacion69@gmail.com>
2024-03-14 23:22:35 -0400

    update

--- pages/components/ProximosTorneos.js
+++ pages/components/ProximosTorneos.js
@@ -34,7 +34,7 @@ export default function ProximosTorneos() {
       <div className="lg:flex gap-6 bg-[#7DCD00] mt-2 p-6 w-auto">
         <div className="flex flex-col gap-4">
           <div className="text-[#DBFFA3] text-sm">
-            Lunes, 1 de Abril a las 7 PM
+            Sabado, 4 de Mayo a las 7 PM
           </div>
           <div className="text-white text-xl font-bold cursor-pointer">
             <Link href="/torneo-clasificatorio-iesf-wec-2024">
@@ -50,7 +50,7 @@ export default function ProximosTorneos() {
         </div>
         <Image
           // animar el icono que brinque arriba y abajo
-          src="/images/flyervmciesfwec24.png"
+          src="/images/ursa.png"
           alt="Venezuela Master CUP"
           width={140}
           height={140}
